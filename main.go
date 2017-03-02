8d58f6a518a6bdf5dab0243c833f9ab03db26e5e
codeskyblue <codeskyblue@gmail.com>
2017-03-02 11:32:20 +0800

    add remote url parse

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"syscall"
+
+	"github.com/codeskyblue/httpfs"
+	"github.com/wmbest2/android/apk"
 )
 
 func ErrToExitCodo(err error) int {
@@ -25,7 +32,91 @@ func ErrToExitCodo(err error) int {
 	return 127
 }
 
+var cmds = map[string]func(...string){
+	"parse": cmdParse,
+}
+
+func readManifestFromZip(zrd *zip.Reader) (data []byte, err error) {
+	for _, f := range zrd.File {
+		if f.Name != "AndroidManifest.xml" {
+			continue
+		}
+		rc, er := f.Open()
+		if er != nil {
+			return nil, er
+		}
+		data, err = ioutil.ReadAll(rc)
+		rc.Close()
+		return
+	}
+	return nil, fmt.Errorf("File not found: AndroidManifest.xml")
+}
+
+func parseManifest(data []byte) {
+	var manifest apk.Manifest
+	err := apk.Unmarshal(data, &manifest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var launchActivity apk.AppActivity
+	for _, act := range manifest.App.Activity {
+		for _, intent := range act.IntentFilter {
+			if intent.Action.Name == "android.intent.action.MAIN" &&
+				intent.Category.Name == "android.intent.category.LAUNCHER" {
+				launchActivity = act
+				goto FOUND
+			}
+		}
+	}
+FOUND:
+	output, _ := json.MarshalIndent(map[string]string{
+		"packageName":    manifest.Package,
+		"launchActivity": launchActivity.Name,
+	}, "", "    ")
+	fmt.Println(string(output))
+	// fmt.Println(manifest.Package)
+	// fmt.Println(launchActivity.Name)
+	// fmt.Printf("adb shell am start -n %s/%s\n", manifest.Package, launchActivity.Name)
+	//out, _ := xml.MarshalIndent(manifest, "", "\t")
+	//fmt.Printf("%s\n", string(out))
+}
+
+func requireAtleastArgs(n int, args []string) {
+	if len(args) != n {
+		log.Fatalf("require at least %d args", n)
+	}
+}
+
+func cmdParse(args ...string) {
+	requireAtleastArgs(1, args)
+
+	url := args[0]
+	file, err := httpfs.Open(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	zrd, err := zip.NewReader(file, file.Size())
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := readManifestFromZip(zrd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parseManifest(data)
+}
+
 func main() {
+	if len(os.Args) > 1 {
+		subCmd := os.Args[1]
+		args := os.Args[2:]
+		fn, ok := cmds[subCmd]
+		if ok {
+			fn(args...)
+			return
+		}
+	}
+
 	cmd := exec.Command("adb", os.Args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
